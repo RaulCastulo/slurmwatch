ac87c889346d11b35fc1523d889cc6f075067a8b
RaulCastulo <rulo.phoenix6@gmail.com>
2017-10-13 15:07:11 -0500

    Se quito codigo innecesario

--- running.py
+++ running.py
@@ -7,8 +7,6 @@ def imprimir(lista):
     for i in lista:
         print i
 
-
-
 def ajustar_output(output):
    
     #Quitamos los espacios en blanco que aparecen al principio de las cadenas
@@ -44,51 +42,6 @@ def agregar_columns_output(output):
     aux.sort(key=itemgetter(3), reverse=True)
     return aux
 
-
-def agregar_colum_output(output):
-    cores = 0
-    enuso = 0
-    carga = 0
-    eff = 0
-    aux = []
-    espcores = 0
-    espuso = 0
-    espcarga = 0
-    espeff = 0
-    espmaxcores = 3
-    espmaxenuso = 3
-    espmaxcarga = 6
-    espmaxeff = 3
-    
-    for i in output:
-        nodos = i.split()[-1]
-        cores, enuso, carga, eff = obtener_carga(nodos)
-        len_cores = len(str(cores))
-        len_enuso = len(str(enuso))
-        len_carga = len(str(carga))
-        len_eff = len(str(eff))
-        if(len_cores < espmaxcores ):
-            espcores = espmaxcores - len_cores
-        else: 
-            espcores = 0
-        if(len_enuso < espmaxenuso):
-            espuso = espmaxenuso - len_enuso
-        else:
-            espuso = 0
-        if(len_carga < espmaxcarga):
-            espcarga = espmaxcarga - len_carga
-        else:
-            espcarga = 0
-        if(len_eff < espmaxeff):
-            espeff = espmaxeff - len_eff
-        else:
-            espeff = 0
-        i = str(cores)+espcores*" "+ "    "+str(enuso)+espuso*" "+"    "+str(carga)+espcarga*" "+"    "+str(eff)+espeff*" "+"    "+i 
-        aux.append(i.split())
-
-
-    return aux
-
 def obtener_carga(nodes):
 
     output = commands.getoutput("scontrol show nodes "+nodes+" | grep CPULoad")
@@ -117,8 +70,6 @@ def obtener_carga(nodes):
     Eff = (Carga/Usados)*100
     #print "Cores: "+str(Cores)+" Usados: "+str(Usados)+" Carga: "+str(Carga)+" %Eff: "+str(int(Eff))
     return Cores, Usados,int(Carga), int(Eff)
-
-
 
 
 cabecera = "CORES INUSE LOAD  %EFF  JOBID       PARTITION   NAME        USER        STATE     TIME        TIME_LIMI   NODES NODELIST(REASON)"
