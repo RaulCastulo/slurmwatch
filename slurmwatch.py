966c3f3e8120bd514f4a40eed61d746bebf1a7d3
RaulCastulo <rulo.phoenix6@gmail.com>
2017-11-27 16:27:23 -0600

    Se agregaron las líneas de código del archivo jobs.py

--- slurmwatch.py
+++ slurmwatch.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 #!/usr/bin/env python
 import curses
@@ -6,11 +5,164 @@ import commands
 import sys
 import os
 import argparse
+from operator import itemgetter
+
 
 # Variables en donde se guardara la informacion que se obtiene de ejecutar los comandos
 global salida  
 global num_lineas
 global lista_salida
+#Haremos uso de esta lista para poder almacenar la informacion de los trabajos
+#Esta lista contendra elementos de tipo string para facilitar la impresion en pantalla
+info = []
+
+# Con esta funcion almacenamos todo en una variable para despues hacer echo y aplicar column -t para que se haga de mejor manera la tabulacion
+# Falta validar bien ya que de momento se rebasa el limite de caracteres aceptados por echo y al final imprime una linea en blanco
+def imprimir_info(lista):
+	"""
+	En esta variable vamos a almacenar toda la informacion de los trabajos en 
+	una sola cadena para poder hacer echo de esta variable mas adelante.
+	"""
+	aux  = ""
+	for i in lista:
+		aux += i+"\n"
+	return aux
+
+
+def imprime_trabajos(lista):
+	informacion = ""
+	for j in lista:
+		cadena = "%-6s%-6s%-6s%-6s%-12s%-12s%-12s%-10s%-10s%-12s%-13s%-6s%-12s" %  (j[0], j[1], j[2], j[3], j[4], j[5], j[6], j[7], j[8], j[9], j[10], j[11],j[12])
+		informacion +=cadena+"\n"
+	return informacion
+
+#Metodo que servira para poder agregar elementos a la lista info
+def agregar_info(lista):
+	
+	for j in lista:
+		lista_aux = []
+		"""
+		Hacemos la conversion de tipo int a tipo string para los primeros 4 elementos
+		de la lista que contiene la informacion de los trabajos en ejecucion, ya que los primeros 
+		4 elementos de esta lista son los valores asociados a CORES, INUSE, LOAD, %EFF
+		"""
+		lista_aux.append(str(j[0]))
+		lista_aux.append(str(j[1]))
+		lista_aux.append(str(j[2]))
+		lista_aux.append(str(j[3]))
+		
+		lista_aux.extend(j[4:])
+
+		info.append(lista_aux)
+		#Esto solo para que funcione al momento de hacer echo de la cadena para obtener mejor tabulacion
+		#cadena = " ".join(lista_aux)
+		#info.append(cadena)
+
+def ajustar_output(output):
+   
+    #Quitamos los espacios en blanco que aparecen al principio de las cadenas
+    
+    valores = []
+    
+    for i in output:
+        i = i.strip()
+        valores.append(i)
+    return valores
+
+def obtener_carga(nodes):
+
+    output = commands.getoutput("scontrol show nodes "+nodes+" | grep CPULoad")
+    valores_loadtemp = output.splitlines()
+
+    valores_load = []
+    #Guardamos en una lista la lista que contiene los valores de CPUAlloc, CPUTot, CPULoad
+    for i in valores_loadtemp:
+        valores_load.append(i.split())
+    
+    Cores = 0
+    Usados = 0
+    Carga = 0
+    Eff = 0
+    secuencia1 = "CPUTot="
+    secuencia2 = "CPUAlloc="
+    secuencia3 = "CPULoad="
+
+    #Recuperamos solo la parte numerica de CPUAlloc, CPUTot, CPULoad
+    for i in valores_load:
+        Cores += int(i[2].lstrip(secuencia1))
+        Usados += int(i[0].lstrip(secuencia2))
+        Carga += float(i[3].lstrip(secuencia3))
+    if(Usados != 0):
+    	Eff = (Carga/Usados)*100
+    else:
+		Eff = 0
+    return Cores, Usados,int(Carga), int(Eff)
+
+
+def agregar_columnas_trabajos_ejecucion(output):
+    cores = 0
+    enuso = 0
+    carga = 0
+    eff = 0
+    aux = []
+    
+    output = ajustar_output(output)
+    
+    for i in output:
+        valores = []
+        valores_output = i.split()
+        nodos = valores_output[-1]
+        cores, enuso, carga, eff = obtener_carga(nodos)
+        valores.append(cores)
+        valores.append(enuso)
+        valores.append(carga)
+        valores.append(eff)
+        valores.extend(valores_output)
+	
+	aux.append(valores)
+	
+    aux.sort(key=itemgetter(3), reverse=True)
+    return aux
+
+def agregar_columnas_trabajos_pendientes(pendientes):
+    pendientes = ajustar_output(pendientes)
+    aux = []
+    for i in pendientes:
+	valores_i = i.split()
+        columnas = ["---", "---", "---", "---"]
+        columnas.extend(valores_i)
+	aux.append(columnas)
+    return aux
+
+def consultar_trabajos(usuario):
+	
+	#Esta variable es util para cuando hacemos echo y column -t 
+	#cabecera = "CORES INUSE LOAD %EFF JOBID PARTITION NAME USER STATE TIME TIME_LIMIT NODES NODELIST(REASON)"
+	cabecera = ["CORES","INUSE","LOAD","%EFF","JOBID","PARTITION","NAME","USER","STATE","TIME","TIME_LIMIT","NODES","NODELIST(REASON)"]
+	info.append(cabecera)
+	informacion_trabajos = ""
+	usuarios = usuario.split()
+	ejecucion = []
+	pendientes = []
+
+	for i in usuarios:
+		ejecucion.extend((commands.getoutput("squeue -h -l -tR -u "+i)).splitlines())
+		pendientes.extend((commands.getoutput("squeue -h -l -tPD -u "+i)).splitlines())
+	
+	if(len(ejecucion) > 0):
+		ejecucion = agregar_columnas_trabajos_ejecucion(ejecucion)
+		agregar_info(ejecucion)
+
+	if(len(pendientes) > 0):
+		pendientes = agregar_columnas_trabajos_pendientes(pendientes)
+		agregar_info(pendientes)
+
+	if(len(info) > 1):
+		informacion_trabajos = imprime_trabajos(info)
+	return informacion_trabajos
+
+
+
 
 def validar_usuario_investigador(usuario, user_id):
 	usuarios = " "
@@ -29,41 +181,6 @@ def validar_usuario_investigador(usuario, user_id):
 		usuarios = usuario
 	return usuarios
 
-# Manejo de parametros que puede recibir al ejecutar el script
-parser = argparse.ArgumentParser()
-parser.add_argument("-A", action="store_true", help="Muestra informacion de trabajos del usuario, colaboradores y alumnos")
-parser.add_argument("-p", action="store_true", help="Imprime la informacion de los trabajos en la terminal")
-
-# Obtenemos los parametros que puede recibir el script
-args = parser.parse_args()
-
-# Validamos los casos posibles al recibir parametros para indicar que hacer en cada caso
-
-if(args.A):
-	usuario = commands.getoutput("echo $USER")
-	user_id = int(commands.getoutput("echo $UID"))
-	usuarios = validar_usuario_investigador(usuario, user_id)
-	if(args.p):
-		trabajos = commands.getoutput("python jobs.py "+usuarios+" | awk '{print $1,$2,$3,$4,$5,$6,$10}' | column -t ")
-		sys.stdout.write(trabajos+"\n")
-		quit()
-	else:
-		trabajos = commands.getoutput("python jobs.py "+usuarios)
-		lista_salida = trabajos.splitlines()
-		num_lineas = str(len(lista_salida) - 1)
-elif(args.p):
-	usuario = commands.getoutput("echo $USER")
-	trabajos = commands.getoutput("python jobs.py "+usuario+" | awk '{print $1,$2,$3,$4,$5,$6,$10}' | column -t ")
-	sys.stdout.write(trabajos+"\n")
-	quit()
-else:
-	usuario = commands.getoutput("echo $USER")
-	trabajos = commands.getoutput("python jobs.py "+usuario)
-	lista_salida = trabajos.splitlines()
-	num_lineas = str(len(lista_salida) - 1)
-
-if(len(lista_salida) == 0):
-	quit()
 
 def inicializar_curses(stdscr, cursor_y, cursor_x):
 
@@ -312,24 +429,64 @@ def crear_pantalla(stdscr):
         k = stdscr.getch()
         if((k == curses.KEY_DOWN) or (k == curses.KEY_UP) or (k == curses.KEY_LEFT) or (k == curses.KEY_RIGHT) or (k == curses.KEY_NPAGE) or (k == 32) or (k == curses.KEY_PPAGE) or (k == curses.KEY_RESIZE)):
             cursor_y, height, width, nlineasup, nlineainf, inilinea, finlinea= sroll(stdscr, k, cursor_y, cursor_x, height, width, nlineasup, nlineainf, inilinea, finlinea, lista_salida)
-        
-	
+
+
  
+# Manejo de parametros que puede recibir al ejecutar el script
+parser = argparse.ArgumentParser()
+parser.add_argument("-A", action="store_true", help="Muestra informacion de trabajos del usuario, colaboradores y alumnos")
+parser.add_argument("-p", action="store_true", help="Imprime la informacion de los trabajos en la terminal")
+
+# Obtenemos los parametros que puede recibir el script
+args = parser.parse_args()
+
+# Validamos los casos posibles al recibir parametros para indicar que hacer en cada caso
+
+if(args.A):
+	usuario = os.getenv('USER')
+	user_id = int(os.getuid())
+	usuarios = validar_usuario_investigador(usuario, user_id)
+	if(args.p):
+		trabajos = consultar_trabajos(usuarios)
+		sys.stdout.write(trabajos+"\n")
+		quit()
+	else:
+		trabajos = consultar_trabajos(usuarios)
+		lista_salida = trabajos.splitlines()
+		num_lineas = str(len(lista_salida) - 1)
+elif(args.p):
+	usuario = os.getenv('USER')
+	trabajos = consultar_trabajos(usuario)
+	sys.stdout.write(trabajos)
+	quit()
+else:
+	usuario = os.getenv('USER')
+	trabajos = consultar_trabajos(usuario)
+	lista_salida = trabajos.splitlines()
+	num_lineas = str(len(lista_salida) - 1)
+
+if(len(lista_salida) == 0):
+	quit()
+
+
+
 def main():
-    stdscr = curses.initscr()
-    height, width = stdscr.getmaxyx()
-    if(height >= 20 and width >= 119):
+	stdscr = curses.initscr()
+	height, width = stdscr.getmaxyx()
+	terminar()
+	sys.stdout.write("alto " +str(height)+" ancho "+str(width)+"\n")
+	if(height >= 20 and width >= 129):
 		curses.wrapper(crear_pantalla)
-    else:
-        if(height < 20 and width < 119):
-            terminar()
-            sys.stdout.write("Tamanio de pantalla insuficiente.....Renglones: "+str(height)+" Columnas: "+str(width)+"\nSe requiere minimo.....Renglones: 20 Columnas: 119"+'\n')
-        if(height >= 20 and width < 119):
-            terminar()
-            sys.stdout.write("Tamanio de pantalla insuficiente.....Renglones: "+str(height)+" Columnas: "+str(width)+"\nSe requiere minimo.....Renglones: 20 Columnas: 119"+'\n')
-        if(height < 20 and width >= 119):
-            terminar()
-            sys.stdout.write("Tamanio de pantalla insuficiente.....Renglones: "+str(height)+" Columnas: "+str(width)+"\nSe requiere minimo.....Renglones: 20 Columnas: 119"+'\n')
+	else:
+		if(height < 20 and width < 129):
+			terminar()
+			sys.stdout.write("Tamanio de pantalla insuficiente.....Renglones: "+str(height)+" Columnas: "+str(width)+"\nSe requiere minimo.....Renglones: 20 Columnas: 129"+'\n')
+		if(height >= 20 and width < 129):
+			terminar()
+			sys.stdout.write("Tamanio de pantalla insuficiente.....Renglones: "+str(height)+" Columnas: "+str(width)+"\nSe requiere minimo.....Renglones: 20 Columnas: 129"+'\n')
+		if(height < 20 and width >= 129):
+			terminar()
+			sys.stdout.write("Tamanio de pantalla insuficiente.....Renglones: "+str(height)+" Columnas: "+str(width)+"\nSe requiere minimo.....Renglones: 20 Columnas: 129"+'\n')
 
 if __name__ == "__main__":
 	"""
